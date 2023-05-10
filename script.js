348ff5488c915139c22a083fceec8dd83f4cffe8
Axelo21 <axel.osterberg@skola.taby.se>
2023-05-10 10:53:18 +0200

    Version 0.4.1

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ let weather = {
         document.querySelector(".temp").innerText = Math.round(temp * 10) / 10 + "°C"
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%"
         document.querySelector(".wind").innerText = "Wind speed: " + Math.round(speed) + " m/s"
-        document.body.style.backgroundImage = 'url(https://source.unsplash.com/1920x1080/?' + name + ")"
 
         document.querySelector(".locationPic").style.backgroundImage = 'url(https://source.unsplash.com/1920x1080/?' + name + ")"
     },
@@ -38,3 +37,52 @@ document.querySelector(" .input-search-bar").addEventListener("keyup", function(
 })
 
 weather.fetchWeather("Vaxholm")
+
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+
+/* Activate Submenu */
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+  if (menu.querySelector(".submenu-active")) {
+    let isClickInside = menu
+      .querySelector(".submenu-active")
+      .contains(e.target);
+
+    if (!isClickInside && menu.querySelector(".submenu-active")) {
+      menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    }
+  }
+}
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
+for (let item of items) {
+  if (item.querySelector(".submenu")) {
+    item.addEventListener("click", toggleItem, false);
+  }
+  item.addEventListener("keypress", toggleItem, false);
+}
+document.addEventListener("click", closeSubmenu, false);
