2df2cf6510d59d650dfbe53b816d2b412d2bc25f
Axelo21 <axel.osterberg@skola.taby.se>
2023-05-21 20:30:00 +0200

    Final

--- JS/script_login_page.js
+++ JS/script_login_page.js
@@ -1,6 +1,7 @@
 let btnLogin = document.getElementById("do-login");
 let idLogin = document.getElementById("login");
 let username = document.getElementById("username");
+//Login message
 btnLogin.onclick = function () {
   idLogin.innerHTML =
     "<p>We're happy to see you again, </p><h1>" + username.value + "</h1>";
